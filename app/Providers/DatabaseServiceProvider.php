d518718dc1e8e7cf20121ab6ee62fa81f06110f8
omegamvc <agisoftt@gmail.com>
2025-10-24 21:10:27 +0200

    Prepare for new Cache package

--- app/Providers/DatabaseServiceProvider.php
+++ app/Providers/DatabaseServiceProvider.php
@@ -9,9 +9,10 @@ use Omega\Container\Exceptions\DependencyException;
 use Omega\Container\Exceptions\NotFoundException;
 use Omega\Container\Provider\AbstractServiceProvider;
 use Omega\Database\DatabaseManager;
-use Omega\Database\MyPDO;
-use Omega\Database\MyQuery;
-use Omega\Database\MySchema;
+use Omega\Database\Connection;
+use Omega\Database\Query\Query;
+use Omega\Database\Schema\Schema;
+use Omega\Database\Schema\SchemaConnection;
 
 class DatabaseServiceProvider extends AbstractServiceProvider
 {
@@ -32,29 +33,29 @@ class DatabaseServiceProvider extends AbstractServiceProvider
         $this->app->set('dsn.sql', $dsn);
 
         $this->app->set(
-            MyPDO::class,
-            fn () => new MyPDO($this->app->get('dsn.sql'))
+            Connection::class,
+            fn () => new Connection($this->app->get('dsn.sql'))
         );
 
         $this->app->set(
-            MySchema\MyPDO::class,
-            fn () => new MySchema\MyPDO($this->app->get('dsn.sql'))
+            SchemaConnection::class,
+            fn () => new SchemaConnection($this->app->get('dsn.sql'))
         );
 
         $this->app->set(
-            'MyQuery',
-            fn () => new MyQuery($this->app->get(MyPDO::class))
+            'Query',
+            fn () => new Query($this->app->get(Connection::class))
         );
 
         $this->app->set(
-            'MySchema',
-            fn () => new MySchema($this->app->get(MySchema\MyPDO::class), $this->app->get('dsn.sql')['database'])
+            'Schema',
+            fn () => new Schema($this->app->get(SchemaConnection::class), $this->app->get('dsn.sql')['database'])
         );
 
         $this->app->set(
             DatabaseManager::class,
             fn () => new DatabaseManager($this->app->get('dsn.connections'))
-                ->setDefaultConnection($this->app->get(MyPDO::class))
+                ->setDefaultConnection($this->app->get(Connection::class))
         );
     }
 }
--- config/cache.php
+++ config/cache.php
@@ -3,5 +3,14 @@
 declare(strict_types=1);
 
 return [
-    'cache' => env('CACHE_STORAGE', '')
+    'cache' => [
+        'default' => env('CACHE_STORAGE', 'file'),
+        'storage' => [
+            'file'      => [],
+            'array'     => [],
+            'memcached' => [],
+            'redis'     => [],
+            'database'  => [],
+        ],
+    ]
 ];
